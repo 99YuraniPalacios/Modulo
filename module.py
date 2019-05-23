4b15c24a9900db2abf06527d1e1d9dafc0eab14a
99YuraniPalacios <yumepa.mp@gmail.com>
2019-05-23 13:42:03 -0500

    Correccion

--- module.py
+++ module.py
@@ -3,27 +3,27 @@ import os
 
 class Series(object):
 
-    def __init__(self,Max,a):
-        self.Max=Max
+    def __init__(self,N,a):
+        self.N=N
         self.a=a
         n=len(a)
         
     def get_value(self,x):
         suma=0
-        for n in range(self.Max):
+        for n in range(self.N):
             suma+=self.a[n]*x**n
         return suma
 
     def Get_derivative(self,x):
         deriv=0
-        for n in range(1,self.Max):
+        for n in range(1,self.N):
             deriv+=n*self.a[n]*x**(n-1)
         return deriv
 
     def GetError(self,x,n):
         if n<0:
             raise ValueError("n should be larger than 0")
-        elif n>self.Max-1:
+        elif n>self.N-1:
             raise ValueError("n should not be larger than {self.N}")
         else:
             error=x**(n+1)
